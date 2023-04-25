c95e7558448a8b1cf266da8ae951b85ecdda5497
John Agni <johncliffordagni@gmail.com>
2023-04-24 16:30:20 -1000

    Made changes to nav also listed recipes working

--- ghi/src/components/Recipes/UserRecipes.jsx
+++ ghi/src/components/Recipes/UserRecipes.jsx
@@ -1,30 +1,82 @@
 import { useContext, useEffect, useState } from "react";
-import { Box, Image, Text, Link } from "@chakra-ui/react";
+import {
+    Box,
+    Image,
+    Text,
+    Link,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    IconButton,
+    useDisclosure,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    Button,
+} from "@chakra-ui/react";
+import { FaEllipsisV } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
+import useUser from "../useUser";
 import useToken from "@galvanize-inc/jwtdown-for-react";
 
-function RecipeCard({ recipe }) {
+function RecipeCard({ recipe, onDelete }) {
     const navigate = useNavigate();
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     const handleClick = () => {
         navigate(`/recipes/${recipe.id}`);
     };
 
+    const handleDelete = () => {
+        onClose();
+        onDelete(recipe.id);
+    };
+
     return (
-        <Box borderWidth="1px" borderRadius="lg" overflow="hidden" p={4} mb={4}>
+        <Box borderWidth="1px" borderRadius="lg" overflow="hidden" p={4} mb={4} position="relative">
+            <Menu>
+                <MenuButton as={IconButton} aria-label="Options" icon={<FaEllipsisV />} position="absolute" top={0} right={0} />
+                <MenuList>
+                    <MenuItem onClick={handleDelete}>Delete Recipe</MenuItem>
+                </MenuList>
+            </Menu>
             <Image src={recipe.image_url} alt={recipe.recipe_name} mb={4} />
             <Text fontSize="xl" fontWeight="bold" mb={2}>
                 {recipe.recipe_name}
             </Text>
             <Text mb={4}>{recipe.description}</Text>
-            <Link onClick={handleClick}>View recipe</Link>
+            <Button onClick={handleClick} mb={2}>
+                View recipe
+            </Button>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Delete Recipe</ModalHeader>
+                    <ModalBody>
+                        Are you sure you want to delete the recipe "{recipe.recipe_name}"?
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3} onClick={handleDelete}>
+                            Delete
+                        </Button>
+                        <Button variant="ghost" onClick={onClose}>
+                            Cancel
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </Box>
     );
 }
 
+
 function UserRecipes() {
     const token = useToken();
-    const [user, setUser] = useState("");
+    const user = useUser(token);
     const [recipes, setRecipes] = useState([]);
     const navigate = useNavigate();
 
@@ -36,12 +88,12 @@ function UserRecipes() {
         });
         if (response.ok) {
             navigate('/my-recipes');
-            fetchUserRecipes();
+            fetchUserRecipes(user.id);
         }
     };
 
-    const fetchUserRecipes = async () => {
-        const url = `${process.env.REACT_APP_USER_SERVICE_API_HOST}/recipes/`;
+    const fetchUserRecipes = async (user_id) => {
+        const url = `${process.env.REACT_APP_USER_SERVICE_API_HOST}/recipes/user/${user_id}`;
         const response = await fetch(url, {
             method: "GET",
             credentials: "include",
@@ -52,39 +104,35 @@ function UserRecipes() {
         }
     };
 
-    const fetchUser = async () => {
-        const url = `${process.env.REACT_APP_USER_SERVICE_API_HOST}/token`;
-        const response = await fetch(url, {
-            method: "GET",
-            credentials: "include",
-        });
-        if (response.ok) {
-            const data = await response.json();
-            setUser(data.account);
+    useEffect(() => {
+        if (user && user.id) {
+            fetchUserRecipes(user.id);
         }
-    };
+    }, [user]);
 
-    useEffect(() => {
-        fetchUser();
-    }, [token]);
-
-    useEffect(() => {
-        fetchUserRecipes(user.id);
-    }, [user])
+    if (!user) {
+        return null;
+    }
 
     return (
         <Box maxW="600px" mx="auto">
-            <Text>
-                {user.name}'s recipes'
+            <Text
+                fontSize="3xl"
+                fontWeight="bold"
+                textAlign="center"
+                my={8}
+            >
+                {user.first_name}'s Recipes
             </Text>
             {recipes.map((recipe) => (
-                <Box key={recipe.id}>
-                    <RecipeCard recipe={recipe} />
-                    <Link onClick={() => handleDelete(recipe.id)}>Delete recipe</Link>
+                <Box key={recipe.id} position="relative">
+                    <RecipeCard recipe={recipe} onDelete={handleDelete} />
                 </Box>
             ))}
         </Box>
     );
 }
+
+
 
 export default UserRecipes;
