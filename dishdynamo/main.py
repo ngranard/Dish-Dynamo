782d33bef4e226ef345175295a1632eae2f992fc
Noah Granard <ngranard@pm.me>
2023-04-28 12:25:32 -0700

    ci.yml change

--- dishdynamo/main.py
+++ dishdynamo/main.py
@@ -15,6 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8000",
         "http://localhost:3000",
+        "https://team-scrumtious.gitlab.io/dish-dynamo",
         os.environ.get("CORS_HOST", None),
     ],
     allow_credentials=True,
